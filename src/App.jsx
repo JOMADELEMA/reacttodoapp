a9fd8d9944c3d2d864a5e91b07432d5d57d3a463
JOMA-OFFICE <jomadelema@hotmail.com>
2022-04-04 16:24:56 -0600

    se incluyo mantine y se implementó el cambio de tema (falta ajustar para que cambie el contenido

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,13 @@ import {
 } from "tabler-icons-react";
 
 import Tarea from "./Components/Tarea";
+import CambiarTema from "./Components/CambiarTema";
+
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
 
 function App() {
   //Estado para almacenar todas las tareas y mostrarlas
@@ -23,6 +30,12 @@ function App() {
   const [tareas, setTareas] = useState([]);
   const [tareasTemp, setTareasTemp] = useState([]);
   const [estadoLista, setEstadoLista] = useState(0); //se pondrán 3 valores 0 = todos, 1 = pendientes y 2 = completados
+
+  const [colorScheme, setColorScheme] = useState("dark");
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+  };
 
   //contador de tareas para el listado todal de tareas
   const [contador, setContador] = useState(0);
@@ -52,7 +65,11 @@ function App() {
     });
   })();
 
-  //funcion para obtener valor del formulario y agregar la tarea al listado
+  /**Obtiene el valor del formulario y agrega la tarea al listado.
+   *
+   * @param {string} e
+   */
+
   const agregarTarea = (e) => {
     e.preventDefault();
 
@@ -72,8 +89,11 @@ function App() {
     }
   };
 
-  //funcion para eliminar la tarea del listado
-  //recibe el id de la tarea que se quiere eliminar
+  /**
+   *funcion para eliminar la tarea del listado, recibe el id de la tarea que se quiere eliminar
+   *
+   * @param {string} idTarea
+   */
   function eliminarTarea(idTarea) {
     let nuevoListado = tareas.filter((tarea) => tarea.id !== idTarea);
     setTareas(nuevoListado);
@@ -215,86 +235,93 @@ function App() {
 
   return (
     <>
-      <div className="container text-center">
-        <h1 className="m-3">Tareas</h1>
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider theme={{ colorScheme }} withGlobalStyles>
+          <CambiarTema />
 
-        <div className="row mt-5 mb-3">
-          <div className="col-1"></div>
-          <div className="col-10">
-            <form
-              className="needs-validation  d-flex justify-content-center"
-              noValidate
-              onSubmit={agregarTarea}
-            >
-              <div className="input-group mb-3 w-75">
-                <input
-                  type="text"
-                  id="nuevaTarea"
-                  className="form-control"
-                  placeholder="Ingrese una Tarea"
-                  required
-                />
-                <button className="btn btn-outline-primary" type="submit">
-                  <Plus size={25} />
+          <div className="container text-center">
+            <h1 className="m-3">Tareas</h1>
+
+            <div className="row mt-5 mb-3">
+              <div className="col-1"></div>
+              <div className="col-10">
+                <form
+                  className="needs-validation  d-flex justify-content-center"
+                  noValidate
+                  onSubmit={agregarTarea}
+                >
+                  <div className="input-group mb-3 w-75">
+                    <input
+                      type="text"
+                      id="nuevaTarea"
+                      className="form-control"
+                      placeholder="Ingrese una Tarea"
+                      required
+                    />
+                    <button className="btn btn-outline-primary" type="submit">
+                      <Plus size={25} />
+                    </button>
+                  </div>
+                </form>
+              </div>
+              <div className="col-1"></div>
+            </div>
+            <hr />
+
+            <div className="row">
+              <div className="col-1"> </div>
+              <div className="col-10 d-flex justify-content-evenly">
+                <button
+                  className="btn btn-outline-primary align-self-center"
+                  onClick={() => {
+                    listarTareas();
+                  }}
+                >
+                  <List className="me-2" size={25} strokeWidth={1} />
+                  Todas
+                </button>
+                <button
+                  className="btn btn-outline-success align-self-center"
+                  onClick={() => {
+                    listarTareasCompletadas();
+                  }}
+                >
+                  <ListCheck className="me-2" size={25} strokeWidth={1} />
+                  Completadas
+                </button>
+                <button
+                  className="btn btn-outline-danger align-self-center"
+                  onClick={() => {
+                    listarTareasPendientes();
+                  }}
+                >
+                  <ListDetails className="me-2" size={25} strokeWidth={1} />
+                  Pendientes
                 </button>
               </div>
-            </form>
-          </div>
-          <div className="col-1"></div>
-        </div>
-        <hr />
+              <div className="col-1"> </div>
+            </div>
 
-        <div className="row">
-          <div className="col-1"> </div>
-          <div className="col-10 d-flex justify-content-evenly">
-            <button
-              className="btn btn-outline-primary align-self-center"
-              onClick={() => {
-                listarTareas();
-              }}
-            >
-              <List className="me-2" size={25} strokeWidth={1}/>
-              Todas
-            </button>
-            <button
-              className="btn btn-outline-success align-self-center"
-              onClick={() => {
-                listarTareasCompletadas();
-              }}
-            >
-              <ListCheck className="me-2" size={25} strokeWidth={1}/>
-              Completadas
-            </button>
-            <button
-              className="btn btn-outline-danger align-self-center"
-              onClick={() => {
-                listarTareasPendientes();
-              }}
-            >
-              <ListDetails className="me-2" size={25} strokeWidth={1}/>
-              Pendientes
-            </button>
-          </div>
-          <div className="col-1"> </div>
-        </div>
-
-        <hr />
-        <div className="row">
-          <div className="col-1"></div>
-          <div className="col-10">
-            <ul className="list-group" id="listadoTareas">
-              {tareas.map((item) => (
-                <>
-                  <div className="d-flex">
-                    <div
-                      className="flex-fill"
-                      onClick={() => {
-                        completarTarea(item.id);
-                      }}
-                    >
-                      <Tarea tarea={item} />
-                    </div>
-                    {/* <button
+            <hr />
+            <div className="row">
+              <div className="col-1"></div>
+              <div className="col-10">
+                <ul className="list-group" id="listadoTareas">
+                  {tareas.map((item) => (
+                    <>
+                      <div className="d-flex">
+                        <div
+                          className="flex-fill"
+                          onClick={() => {
+                            completarTarea(item.id);
+                          }}
+                        >
+                          <Tarea tarea={item} />
+                        </div>
+                        {/* <button
                       className="btn btn-outline-success"
                       onClick={() => {
                         completarTarea(item.id);
@@ -302,74 +329,77 @@ function App() {
                     >
                       <Check size={25} />
                     </button> */}
-                    <button
-                      className="btn btn-outline-danger"
-                      onClick={() => {
-                        eliminarTarea(item.id);
-                      }}
-                    >
-                      {" "}
-                      <X size={25} strokeWidth={1}/>
-                    </button>
-                  </div>
-                </>
-              ))}
-            </ul>
-          </div>
-          <div className="col-1"></div>
-        </div>
+                        <button
+                          className="btn btn-outline-danger"
+                          onClick={() => {
+                            eliminarTarea(item.id);
+                          }}
+                        >
+                          {" "}
+                          <X size={25} strokeWidth={1} />
+                        </button>
+                      </div>
+                    </>
+                  ))}
+                </ul>
+              </div>
+              <div className="col-1"></div>
+            </div>
 
-      <div className="row mt-5">
-          <div className="col-1"></div>
-          <div className="col-10 d-flex justify-content-evenly">
-            <button
-              className="btn btn-outline-info align-self-center"
-              onClick={() => {
-                marcarTodasCompletas();
-              }}
-            >
-              <SquareCheck className="me-2" size={25} strokeWidth={1}/>
-              Marcar Todas
-            </button>
-            <button
-              className="btn btn-outline-secondary align-self-center"
-              onClick={() => {
-                marcarTodasPendientes();
-              }}
-            >
-              <Square className="me-2" size={25} strokeWidth={1} />
-              Desmarcar Todas
-            </button>
+            <div className="row mt-5">
+              <div className="col-1"></div>
+              <div className="col-10 d-flex justify-content-evenly">
+                <button
+                  className="btn btn-outline-info align-self-center"
+                  onClick={() => {
+                    marcarTodasCompletas();
+                  }}
+                >
+                  <SquareCheck className="me-2" size={25} strokeWidth={1} />
+                  Marcar Todas
+                </button>
+                <button
+                  className="btn btn-outline-secondary align-self-center"
+                  onClick={() => {
+                    marcarTodasPendientes();
+                  }}
+                >
+                  <Square className="me-2" size={25} strokeWidth={1} />
+                  Desmarcar Todas
+                </button>
+              </div>
+              <div className="col-1"></div>
+            </div>
           </div>
-          <div className="col-1"></div>
-        </div>
-      </div>
 
-
-      <footer className="footer mt-5 py-3">
-        <div className="row flex-fill text-center align-self-baseline">
-          <div className="col">
-            <h4>
-              Tareas Totales:
-              {tareas.length > 0 ? (
-                <span className="text-danger fw-bold">{tareas.length}</span>
-              ) : (
-                <span className="text-success fw-bold">{tareas.length}</span>
-              )}
-            </h4>
-          </div>
-          <div className="col">
-            <h4>
-              Tareas Pendientes:
-              {pendientes > 0 ? (
-                <span className="text-danger fw-bold">{pendientes}</span>
-              ) : (
-                <span className="text-success fw-bold">{pendientes}</span>
-              )}
-            </h4>
-          </div>
-        </div>
-      </footer>
+          <footer className="footer mt-5 py-3">
+            <div className="row flex-fill text-center align-self-baseline">
+              <div className="col">
+                <h4>
+                  Tareas Totales:
+                  {tareas.length > 0 ? (
+                    <span className="text-danger fw-bold">{tareas.length}</span>
+                  ) : (
+                    <span className="text-success fw-bold">
+                      {tareas.length}
+                    </span>
+                  )}
+                </h4>
+              </div>
+              <div className="col">
+                <h4>
+                  Tareas Pendientes:
+                  {pendientes > 0 ? (
+                    <span className="text-danger fw-bold">{pendientes}</span>
+                  ) : (
+                    <span className="text-success fw-bold">{pendientes}</span>
+                  )}
+                </h4>
+              </div>
+            </div>
+          </footer>
+        </MantineProvider>
+      </ColorSchemeProvider>
     </>
   );
 }
